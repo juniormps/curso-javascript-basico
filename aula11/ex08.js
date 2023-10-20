1a69cb452fc887ba576a15eb87f733cd05d72fd6
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2023-10-20 17:55:45 -0300

    Aula 11 - ex08 e ex09
    Exemplos de condição simples e condição composta com JavaScript.

---
+++ aula11/ex08.js
@@ -0,0 +1,8 @@
+//Exemplo de CONDIÇÃO SIMPLES.
+
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) {
+	console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex09.js
@@ -0,0 +1,9 @@
+//Exemplo de CONDIÇÃO COMPOSTA.
+
+var país = 'França'
+console.log(`Seu país de origem é ${país}`)
+if (país == 'Brasil') {
+	console.log(`Portanto, você é brasileiro!`)
+} else {
+	console.log(`Portanto, você é estrangeiro!`)	
+}
